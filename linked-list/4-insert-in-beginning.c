452ac1a18b98c46037cceeb65edd4635c8f3fcfd
ayush samor <ayushsamor21@gmail.com>
2025-07-05 15:30:00 +0530

    insert after value

---
+++ linked-list/4-insert-in-beginning.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value){
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = value;
+    node->next = NULL;
+    return node;
+}
+
+struct Node* append(struct Node* head, int value){
+    struct Node* newNode = createNode(value);
+    if (head == NULL){                        
+        head = newNode;
+        return head;
+    }
+    struct Node* temp;                       
+    temp = head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode; 
+    return head;
+}
+
+void printLinkedList(struct Node* head){
+    struct Node* temp = head;
+    while (temp != NULL){
+        printf("%d", temp->data);
+        if (temp->next != NULL){
+            printf("->");
+        }
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+struct Node* insertInBeginning(struct Node* head){
+    int element;
+    printf("Please enter the element: ");
+    scanf("%d", &element);
+
+    struct Node* newNode = createNode(element);
+    newNode->next = head;
+
+    return newNode;
+}
+
+void freeList(struct Node* head){
+    struct Node* temp;
+    while (head != NULL){
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(){
+    int times, num;
+
+    printf("Please enter the number of elements : ");
+    scanf("%d", &times);
+
+    struct Node* head = NULL;
+    for(int i = 0; i < times; i++){
+        printf("Please enter the elements : ");
+        scanf("%d", &num);
+        head = append(head, num);
+    }
+    printf("Original Linked List: ");
+    printLinkedList(head);
+
+    head = insertInBeginning(head);
+    printf("New Linked List: ");
+    printLinkedList(head);
+
+    freeList(head);
+}

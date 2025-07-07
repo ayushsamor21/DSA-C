d8be5efe41a6a6f59eced2b2500e116bf056e63f
ayush samor <ayushsamor21@gmail.com>
2025-07-07 09:41:51 +0530

    reverse printing linked list

---
+++ linked-list/13-reverse-linked-list.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value){
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = value;
+    node->next = NULL;
+    return node;
+}
+
+struct Node* append(struct Node* head, int value){
+    struct Node* newNode = createNode(value);
+    if (head == NULL){                        
+        head = newNode;
+        return head;
+    }
+    struct Node* temp;                       
+    temp = head;
+    while (temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = newNode; 
+    return head;
+}
+
+void printLinkedList(struct Node* head){
+    struct Node* temp = head;
+    while (temp != NULL){
+        printf("%d", temp->data);
+        if (temp->next != NULL){
+            printf("->");
+        }
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void reverseLinkedList(struct Node* head){
+    struct Node* temp = head;
+    if (temp == NULL){
+        return;
+    }
+    reverseLinkedList(temp->next);
+    printf("%d->", temp->data);
+}
+
+void freeList(struct Node* head){
+    struct Node* temp;
+    while (head != NULL){
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(){
+    int times, num;
+
+    printf("Please enter the number of elements : ");
+    scanf("%d", &times);
+
+    struct Node* head = NULL;
+    for(int i = 0; i < times; i++){
+        printf("Please enter the elements %d: ", i + 1);
+        scanf("%d", &num);
+        head = append(head, num);
+    }
+
+    printf("The Original linked list: ");
+    printLinkedList(head);
+    printf("\n");
+
+    reverseLinkedList(head);
+
+    freeList(head);
+}
